d203266640b9785cc8e46e3e2b23c1a733dc17c8
Luiz Felipe Fontes Botelho <luizfelipefb@gmail.com>
2026-04-07 22:53:52 -0300

    feat(nvim): add snacks picker config to show hidden and ignored files

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        hidden = true,
+        ignored = true,
+        sources = {
+          files = {
+            hidden = true,
+            ignored = true,
+          },
+          explorer = {
+            hidden = true,
+            ignored = true,
+          },
+        },
+      },
+    },
+  },
+}
+
